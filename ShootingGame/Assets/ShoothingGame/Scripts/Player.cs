fc43016dbbca6d59a57ddc81aa94a94e7f4a9944
godowon <lakun0987@gmail.com>
2020-07-20 21:07:08 +0900

    gg

---
+++ ShootingGame/Assets/ShoothingGame/Scripts/Player.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float m_Speed = 50f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+
+        float xAxis = Input.GetAxis("Horizontal") * m_Speed * Time.deltaTime;
+        float yAxis = Input.GetAxis("Vertical") * m_Speed * Time.deltaTime;
+        this.transform.Translate(new Vector3(xAxis, yAxis, 0));
+    }
+}
