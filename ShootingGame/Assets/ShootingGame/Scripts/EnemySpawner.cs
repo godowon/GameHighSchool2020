ad3c3ab15edb788d209b0b02efbcaf7e8b1cbb11
godowon <lakun0987@gmail.com>
2020-07-27 21:13:25 +0900

    gg

---
+++ ShootingGame/Assets/ShootingGame/Scripts/EnemySpawner.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    public Transform[] m_SpawnPoints;
+    public GameObject m_EnemyPrefab;
+    public Animator m_Animator;
+
+    public float m_SapwnIntervalMin = 2f;
+    public float m_SapwnIntervalMax = 6f;
+
+    public int m_MinSpawnCount = 1;
+    public int m_MaxSpawnCount = 4;
+    public bool isDead;
+
+    public float m_SpawnCooldown = 0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_SpawnCooldown = Random.Range(m_SapwnIntervalMin, m_SapwnIntervalMax);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(m_SpawnCooldown <= 0)
+        {
+            int spawnCout = Random.Range(m_MinSpawnCount, m_MaxSpawnCount);
+
+            List<int> spawnNums = new List<int>();
+            for (int i = 0; i < spawnCout; i++)
+            {
+                int spawnNum;
+                do
+                {
+                    spawnNum = Random.Range(0, m_SpawnPoints.Length);
+                }
+                while (spawnNums.Contains(spawnNum));
+
+                spawnNums.Add(spawnNum);
+            }
+            foreach (var spawnNum in spawnNums)
+            {
+                var eulerAngle = m_SpawnPoints[spawnNum].eulerAngles += Vector3.up * Random.Range(-15f, 15f);
+
+                GameObject bullet = GameObject.Instantiate(m_EnemyPrefab, m_SpawnPoints[spawnNum].position, Quaternion.Euler(eulerAngle));
+            }
+            m_SpawnCooldown = Random.Range(m_SapwnIntervalMin, m_SapwnIntervalMax);
+        }
+        m_SpawnCooldown -= Time.deltaTime;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag == "PlayerBullet")
+        {
+            m_Animator.SetBool("Die", true);
+
+            GameManager.instance.AddScore();
+            isDead = true;
+        }
+    }
+    public void Die()
+    {
+        Destroy(gameObject);
+    }
+}
---
+++ ShootingGame/Assets/ShootingGame/Scripts/GameManager.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public UnityEngine.UI.Text m_ScoreUI;
+    public int m_Score = 0;
+
+    public bool m_IsGameOver;
+
+    public static GameManager instance;
+
+    public void Awake()
+    {
+        if (instance == null)
+            instance = this;
+        else
+            Destroy(gameObject);
+    }
+
+    public void AddScore()
+    {
+        m_Score += 10;
+        m_ScoreUI.text = "Score : " + m_Score;
+    }
+
+    public void OnPlayerDie()
+    {
+        m_IsGameOver = true;
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    public void Update()
+    {
+        if(m_IsGameOver && Input.GetKeyDown(KeyCode.R))
+        {
+            SceneManager.LoadScene(0);
+        }
+    }
+}
