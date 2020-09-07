e45a637fc5e16e8cd8afa5d145f79841f01e157b
godowon <lakun0987@gmail.com>
2020-09-07 21:14:43 +0900

    gg

--- SunnyLand/Assets/GameSchool/Scripts/HPComponent.cs
+++ SunnyLand/Assets/GameSchool/Scripts/HPComponent.cs
@@ -1,18 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class HPComponent : MonoBehaviour
 {
     // Start is called before the first frame update
+    public UnityEvent m_OnDie;
+    public UnityEvent m_OnTakeDamage;
+    public UnityEvent m_OnTakeHeal;
+
     void Start()
     {
-        
+        m_OnTakeDamage.Invoke();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Destroy(gameObject);
     }
 }
---
+++ SunnyLand/Assets/GameSchool/Scripts/JoinCamera.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JoinCamera : MonoBehaviour
+{
+    public Transform m_Target;
+    public Vector3 m_Offset;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = m_Target.position + m_Offset;
+    }
+}
